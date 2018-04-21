d3e9b1c4e17195b4781e58cf22ce4341b301cca4
Petr Luzhin <lithx@yandex.com>
2018-04-21 21:14:30 +0300

    Remove 'search' method from ClientInterface

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -4,20 +4,4 @@ namespace DesuProject\ChanbooruInterface;
 
 interface ClientInterface
 {
-    /**
-     * Search posts.
-     *
-     * @param string[] $tags  array of tags
-     * @param int|null $page  Page number. If `null`,
-     *                        first page will be requested.
-     * @param int|null $limit How much posts to show. If `null`,
-     *                        default imageboard's limit will be used.
-     *
-     * @return PostInterface[]
-     */
-    public function search(
-        array $tags = [],
-        ?int $page = null,
-        ?int $limit = null
-    ): array;
 }
