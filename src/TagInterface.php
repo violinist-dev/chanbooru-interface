805b583ebb6d59e74c7aa13f84b47c4a0147456c
Petr Flaks <lithx@yandex.com>
2018-06-10 19:02:35 +0300

    Add 'general' tag type

--- src/TagInterface.php
+++ src/TagInterface.php
@@ -4,11 +4,12 @@ namespace DesuProject\ChanbooruInterface;
 
 interface TagInterface
 {
-    const TYPE_TITLE = 1;
-    const TYPE_CHARACTER = 2;
-    const TYPE_ARTIST = 3;
-    const TYPE_META = 4;
-    const TYPE_MODEL = 5;
+    const TYPE_GENERAL = 1;
+    const TYPE_TITLE = 2;
+    const TYPE_CHARACTER = 3;
+    const TYPE_ARTIST = 4;
+    const TYPE_META = 5;
+    const TYPE_MODEL = 6;
 
     /**
      * Tag's title.
