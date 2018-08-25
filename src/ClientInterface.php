7f6331f5605d362007fa356c438e2b36a6f82c68
Petr Flaks <lithx@yandex.com>
2018-08-25 19:58:41 +0300

    Remove ClientInterface argument from ClientInterface methods

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -11,15 +11,13 @@ interface ClientInterface
     /**
      * Search posts.
      *
-     * @param ClientInterface $client
-     * @param string[]        $tags
-     * @param int             $page
-     * @param int             $limit
+     * @param string[] $tags
+     * @param int      $page
+     * @param int      $limit
      *
      * @return PostInterface[]
      */
     public function searchPosts(
-        ClientInterface $client,
         array $tags,
         int $page,
         int $limit = 20
@@ -28,31 +26,27 @@ interface ClientInterface
     /**
      * Get certain post by its ID or throw exception if nothing found.
      *
-     * @param ClientInterface $client
-     * @param int             $id
+     * @param int $id
      *
      * @return PostInterface
      *
      * @throws PostNotFoundException if post not found
      */
     public function getPostById(
-        ClientInterface $client,
         int $id
     ): PostInterface;
 
     /**
-     * @param ClientInterface   $client
      * @param array|null|string $names   If it's string, it will search by pattern.
      *                                   If it's array, it will search strictly by tag names.
-     * @param string            $orderBy
-     * @param bool              $hideEmpty
+     * @param string $orderBy
+     * @param bool   $hideEmpty
      *
      * @return TagInterface[]
      *
      * @throws InvalidArgumentException if invalid $names argument passed
      */
     public function searchTags(
-        ClientInterface $client,
         $names,
         string $orderBy,
         bool $hideEmpty = true
@@ -61,14 +55,12 @@ interface ClientInterface
     /**
      * Search tags by names.
      *
-     * @param ClientInterface $client
-     * @param string[]        $names
-     * @param string          $orderBy
+     * @param string[] $names
+     * @param string   $orderBy
      *
      * @return TagInterface[]
      */
     public function searchTagsByNames(
-        ClientInterface $client,
         array $names,
         string $orderBy
     ): array;
@@ -76,14 +68,12 @@ interface ClientInterface
     /**
      * Search tags by pattern.
      *
-     * @param ClientInterface $client
-     * @param string          $namePattern @example: girl*
-     * @param string          $orderBy
+     * @param string $namePattern @example: girl*
+     * @param string $orderBy
      *
      * @return TagInterface[]
      */
     public function searchTagsByNamePattern(
-        ClientInterface $client,
         string $namePattern,
         string $orderBy
     ): array;
@@ -91,15 +81,13 @@ interface ClientInterface
     /**
      * Search tag by name or throw exception if nothing found.
      *
-     * @param ClientInterface $client
-     * @param string          $name
+     * @param string $name
      *
      * @return TagInterface
      *
      * @throws TagNotFoundException if tag not found
      */
     public function searchTagByName(
-        ClientInterface $client,
         string $name
     ): TagInterface;
 }
